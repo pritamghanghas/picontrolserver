e1dceb831bc27e52af39be7b728b63c0dd1ec052
Pritam Ghanghas <pritam.ghanghas@itaas.com>
2015-07-27 23:39:03 +0530

    moved logging to tmp

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=/usr/local/lib/
 cd /home/pi/picontrolserver/
-/home/pi/picontrolserver/picontrolserver > /home/pi/picontrolserver/picontrolserver.out 
+/home/pi/picontrolserver/picontrolserver > /tmp/picontrolserver.out 
 cd -
