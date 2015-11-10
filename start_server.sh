b2de52e73c0b18b1d78a0ad30e56af82ad303da1
Pritam Ghanghas <pritam.ghanghas@itaas.com>
2015-11-10 23:25:15 +0530

    commented out autostarting of ardupilot

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #start Ardupilot
-/home/pi/ardupilot/ArduCopter/ArduCopter.elf -A udp:127.0.0.1:1440 -B /dev/ttyAMA0 > /home/pi/arducopter.log 2>&1 &
+#/home/pi/ardupilot/ArduCopter/ArduCopter.elf -A udp:127.0.0.1:1440 -B /dev/ttyAMA0 > /home/pi/arducopter.log 2>&1 &
 
 # start control server
 export LD_LIBRARY_PATH=/usr/local/lib/
